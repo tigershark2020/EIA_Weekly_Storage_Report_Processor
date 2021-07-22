5f8d42fb2a0a585aa619e1c20e58b50acf241abc
tigershark2020 <64547173+tigershark2020@users.noreply.github.com>
2021-07-22 07:23:20 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-using MySql.Data.MySqlClient;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-
 namespace EIA_Weekly_Storage_Report_Processor
 {
     class Program
@@ -31,11 +24,11 @@ namespace EIA_Weekly_Storage_Report_Processor
 
         public class EIA_Storage_Data
         {
-            public string ReportStub {get; set;}
+            public string ReportStub { get; set; }
             public string CurrentWeek { get; set; }
             public string WeekAgo { get; set; }
             public string WeekAgo_Difference { get; set; }
-            public string WeekAgo_PercentageChange {get; set;}
+            public string WeekAgo_PercentageChange { get; set; }
             public string YearAgo { get; set; }
             public string YearAgo_DifferenceAmount { get; set; }
             public string YearAgo_PercentChange { get; set; }
@@ -56,51 +49,45 @@ namespace EIA_Weekly_Storage_Report_Processor
         public class EIA_Storage_Data_Report
         {
             public EIA_Storage_Data Header_Data { get; set; }
-            public List<EIA_Storage_Data_Values> Values { get; set; }
+            public System.Collections.Generic.List<EIA_Storage_Data_Values> Values { get; set; }
         }
 
         public static string Download_Report_Text(string url)
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            System.Net.HttpWebRequest request = (System.Net.HttpWebRequest)System.Net.WebRequest.Create(url);
+            System.Net.HttpWebResponse response = (System.Net.HttpWebResponse)request.GetResponse();
 
-            StreamReader sr = new StreamReader(response.GetResponseStream());
+            System.IO.StreamReader sr = new System.IO.StreamReader(response.GetResponseStream());
             string results = sr.ReadToEnd();
             sr.Close();
 
             return results;
         }
 
-        public static void Add_Event_Notifications(ConfigFiles jsonConfigPaths, List<Event_Notification> eventNotificationList)
+        public static void Add_Event_Notifications(ConfigFiles jsonConfigPaths, System.Collections.Generic.List<Event_Notification> eventNotificationList)
         {
-
-            MySql.Data.MySqlClient.MySqlConnection conn;
-
-            conn = new MySql.Data.MySqlClient.MySqlConnection();
-
-            MySqlConnectionStringBuilder conn_string_builder = new MySqlConnectionStringBuilder();
+            MySql.Data.MySqlClient.MySqlConnectionStringBuilder conn_string_builder = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder();
             string json = System.IO.File.ReadAllText(jsonConfigPaths.mysqlCredentialsInsert_JsonFile);
-            conn_string_builder = JsonConvert.DeserializeObject<MySqlConnectionStringBuilder>(json);
+            conn_string_builder = Newtonsoft.Json.JsonConvert.DeserializeObject<MySql.Data.MySqlClient.MySqlConnectionStringBuilder>(json);
 
-            conn = new MySqlConnection(conn_string_builder.ToString());
+            MySql.Data.MySqlClient.MySqlConnection conn = new MySql.Data.MySqlClient.MySqlConnection(conn_string_builder.ToString());
             try
             {
                 conn.Open();
             }
-            catch (Exception erro)
+            catch (System.Exception erro)
             {
-                Console.WriteLine(erro);
+                System.Console.WriteLine(erro);
             }
 
             foreach (Event_Notification eventNotification in eventNotificationList)
             {
                 try
                 {
-                    MySqlCommand cmd = conn.CreateCommand();
+                    MySql.Data.MySqlClient.MySqlCommand cmd = conn.CreateCommand();
                     cmd.Connection = conn;
 
                     cmd.CommandText = "INSERT INTO `geo_data`.`geo_events` (`geo_event_agency`,`geo_event_title`,`geo_event_url`,`geo_event_starttime`,`geo_event_category`,`geo_event_type`,`geo_event_ident`,`geo_event_location_latitude`,`geo_event_location_longitude`,`geo_event_notify`) VALUES (@event_notification_agency,@event_notification_title,@event_notification_url,FROM_UNIXTIME(@event_notification_datetime),@event_notification_category,@event_notification_type,@event_notification_ident,@event_notification_latitude,@event_notification_longitude,1);";
-                    // cmd.Prepare();
 
                     cmd.Parameters.AddWithValue("@event_notification_agency", eventNotification.eventNotification_Agency);
                     cmd.Parameters.AddWithValue("@event_notification_title", eventNotification.eventNotification_Title);
@@ -111,17 +98,17 @@ namespace EIA_Weekly_Storage_Report_Processor
                     cmd.Parameters.AddWithValue("@event_notification_ident", eventNotification.eventNotification_UniqueID);
                     cmd.Parameters.AddWithValue("@event_notification_latitude", eventNotification.eventNotification_Latitude);
                     cmd.Parameters.AddWithValue("@event_notification_longitude", eventNotification.eventNotification_Longitude);
-
+                    cmd.Prepare();
                     cmd.ExecuteNonQuery();
                 }
-                catch (MySqlException error_message)
+                catch(MySql.Data.MySqlClient.MySqlException error_message)
                 {
                     int errorcode = error_message.Number;
-                    Console.WriteLine(errorcode + "\t" + error_message.Message);
+                    System.Console.WriteLine(errorcode + "\t" + error_message.Message);
                 }
-                catch (Exception e)
+                catch (System.Exception e)
                 {
-                    Console.WriteLine(e);
+                    System.Console.WriteLine(e);
                 }
             }
 
@@ -142,18 +129,18 @@ namespace EIA_Weekly_Storage_Report_Processor
             int counter = 0;
             bool continue_status = true;
 
-            List<EIA_Storage_Data_Values> dataList = new List<EIA_Storage_Data_Values>();
+            System.Collections.Generic.List<EIA_Storage_Data_Values> dataList = new System.Collections.Generic.List<EIA_Storage_Data_Values>();
 
             foreach (string item in reportLinesArray)
             {
-                if(continue_status == true)
+                if (continue_status == true)
                 {
                     if (counter > 0)
                     {
-                        String[] dataArray = item.Split("\",\"");
+                        string[] dataArray = item.Split("\",\"");
 
                         string reportStubString = dataArray[0].Replace("\"", "");
- 
+
                         if (reportStubString.Replace("\"", "").Contains("STUB_1"))
                         {
                             continue_status = false;
@@ -165,11 +152,11 @@ namespace EIA_Weekly_Storage_Report_Processor
 
                             dataItem.CurrentWeek = dataArray[1].Replace("\"", "");
                             dataItem.WeekAgo = dataArray[2].Replace("\"", "");
-                            dataItem.WeekAgo_Difference = Double.Parse(dataArray[3].Replace("\"", "").Replace(",", "").Trim());
-                            dataItem.WeekAgo_PercentageChange = Double.Parse(dataArray[4].Replace("\"", "").Replace(",", "").Trim());
+                            dataItem.WeekAgo_Difference = double.Parse(dataArray[3].Replace("\"", "").Replace(",", "").Trim());
+                            dataItem.WeekAgo_PercentageChange = double.Parse(dataArray[4].Replace("\"", "").Replace(",", "").Trim());
                             dataItem.YearAgo = dataArray[5].Replace("\"", "");
-                            dataItem.YearAgo_DifferenceAmount = Double.Parse(dataArray[6].Replace("\"", "").Replace(",", "").Trim());
-                            dataItem.YearAgo_PercentChange = Double.Parse(dataArray[7].Replace("\"", "").Replace(",", "").Trim());
+                            dataItem.YearAgo_DifferenceAmount = double.Parse(dataArray[6].Replace("\"", "").Replace(",", "").Trim());
+                            dataItem.YearAgo_PercentChange = double.Parse(dataArray[7].Replace("\"", "").Replace(",", "").Trim());
 
                             dataList.Add(dataItem);
                             StorageReport.Values = dataList;
@@ -180,7 +167,7 @@ namespace EIA_Weekly_Storage_Report_Processor
                     }
                     else
                     {
-                        String[] headerArray = item.Split("\",\"");
+                        string[] headerArray = item.Split("\",\"");
 
                         EIA_Storage_Data header = new EIA_Storage_Data();
                         header.ReportStub = headerArray[0].Replace("\"", "");
@@ -198,9 +185,9 @@ namespace EIA_Weekly_Storage_Report_Processor
                 counter++;
             }
 
-            List<Event_Notification> notifications = new List<Event_Notification>();
+            System.Collections.Generic.List<Event_Notification> notifications = new System.Collections.Generic.List<Event_Notification>();
 
-            Console.WriteLine("Weekly Report\t " + StorageReport.Header_Data.WeekAgo_Difference + "\t" + StorageReport.Header_Data.WeekAgo_PercentageChange);
+            System.Console.WriteLine("Weekly Report\t " + StorageReport.Header_Data.WeekAgo_Difference + "\t" + StorageReport.Header_Data.WeekAgo_PercentageChange);
 
             foreach (EIA_Storage_Data_Values storageColumnData in StorageReport.Values)
             {
@@ -211,15 +198,15 @@ namespace EIA_Weekly_Storage_Report_Processor
                     if (storageColumnData.WeekAgo_Difference < 0)
                     {
                         title = "Storage Drop Week Over Week";
-                        Console.WriteLine(title);
+                        System.Console.WriteLine(title);
                     }
                     else
                     {
                         title = "Storage Gain Week Over Week";
-                        Console.WriteLine(title);
+                        System.Console.WriteLine(title);
                     }
 
-                    long unixTimestamp = (long)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                    long unixTimestamp = (long)(System.DateTime.UtcNow.Subtract(new System.DateTime(1970, 1, 1))).TotalSeconds;
                     energyNotification.eventNotification_Agency = "48970";
                     energyNotification.eventNotification_Title = title;
                     energyNotification.eventNotification_DatetimeEpoch = unixTimestamp;
@@ -228,27 +215,27 @@ namespace EIA_Weekly_Storage_Report_Processor
                     energyNotification.eventNotification_Category = "Energy";
                     energyNotification.eventNotification_Type = "Economic Data";
                     notifications.Add(energyNotification);
-                    Console.WriteLine(storageColumnData.ReportStub + "\t" + storageColumnData.WeekAgo_Difference + "\t" + storageColumnData.WeekAgo_PercentageChange);
+                    System.Console.WriteLine(storageColumnData.ReportStub + "\t" + storageColumnData.WeekAgo_Difference + "\t" + storageColumnData.WeekAgo_PercentageChange);
                 }
             }
 
             string configFilePaths = "filePaths.json";
-            bool exists = File.Exists(configFilePaths);
+            bool exists = System.IO.File.Exists(configFilePaths);
             string json = null;
 
             try
             {
                 json = System.IO.File.ReadAllText(configFilePaths, System.Text.Encoding.UTF8);
             }
-            catch (Exception json_read)
+            catch (System.Exception json_read)
             {
-                Console.WriteLine(json_read.Message);
+                System.Console.WriteLine(json_read.Message);
             }
 
             if (json != null) // Check That JSON String Read Above From File Contains Data
             {
                 ConfigFiles jsonConfigPaths = new ConfigFiles();
-                jsonConfigPaths = JsonConvert.DeserializeObject<ConfigFiles>(json);
+                jsonConfigPaths = Newtonsoft.Json.JsonConvert.DeserializeObject<ConfigFiles>(json);
                 Add_Event_Notifications(jsonConfigPaths, notifications);
             }
 
